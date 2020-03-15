60915c80e771db89e79dfde28bfb6aba2a15ff2e
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 18:59:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,5 +1,12 @@
 def third_challenge
-
+ epic_tragedy = {
+    :montague => {:patriarch => {},:matriarch => {},:hero => {},:hero_friends => []
+    
+    },
+    
+    :capulet => {:patriarch =>{},:matriarch => {},:heroine => {},:heroine_friends => []
+  }
+}
 
   
 end
